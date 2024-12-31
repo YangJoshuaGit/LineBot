0a253f0ccdd209a40dab12ea4c387c1b82bb2422
YangJoshuaGit <97064ms2.ckps.hc.edu.tw@gmail.com>
2024-12-31 11:30:02 +0800

    delete sqlite version src

--- src/param.py
+++
@@ -1,6 +0,0 @@
-global EXPENDITURE_INCOME
-global MODE
-global MONTH
-EXPENDITURE_INCOME = 0 # expenditrue = 0, income = 1
-MODE = 0 # 0 = Normal, 1 = Accouting, 2 = Audit, 3 = Delete
-MONTHS = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
